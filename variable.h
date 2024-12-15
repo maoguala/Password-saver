20041aecf7570798235879c198c798c45c11ea6b
DESKTOP-N80N1GI <4b3g0123@office.stust.edu.tw>
2024-12-16 03:50:24 +0800

    Sketch

---
+++ variable.h
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <fstream> // Only can used create file but if file path not exists it will faild for action
+#include <iomanip> // IO format setup
+#include <filesystem> // Create file Path if doesn't exist,but it can't write the files  
+#include <ctime> // system time
+#include <windows.h> //Windows API
+//#include <unistd.h> //Unix and Linux API
+
+//std::string log_File_Path = "file_log.log";
+std::ifstream in;
+std::ofstream out;
+
+time_t log_wrote_time = time(0); //Get the time from now
+struct tm *ltm = localtime(&log_wrote_time); //Get the structure from time of local
+
+std::string Log_Directory = "./Log/";
+std::string Log_file = Log_Directory + "pswd.log";
+
+std::string str_password = ""; //saved password
+std::string check_str_password = ""; //check password
+int times_for_locked = 3; //3 times for opportunity and for try to lock the secure-lock 
+char option; // CLI of option 
+/*
+ Note:
+ "return 0" is normal status
+ "return 1" is error status
+*/ 
